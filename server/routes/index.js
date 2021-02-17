1a3f3b9c8b853665d0dcf6d6dd9d15d80b5b7ffc
ElkinDev <elkinmendoza00@gmail.com>
2021-02-17 13:35:09 +1100

    allows entrance to api

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,21 +3,7 @@ const db_shoppin_cart = require('../db');
 const router = express.Router();
 
 router.get('/', async (req,res,next)=>{
-           // Website you wish to allow to connect
-        //    res.setHeader('Access-Control-Allow-Origin', '*');
 
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-
-
-           // Request methods you wish to allow
-           res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-       
-           // Request headers you wish to allow
-           res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-       
-           // Set to true if you need the website to include cookies in the requests sent
-           // to the API (e.g. in case you use sessions)
-           res.setHeader('Access-Control-Allow-Credentials', true);
 
     try {
         let results = await db_shoppin_cart.all();
