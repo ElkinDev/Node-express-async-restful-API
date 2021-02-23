f4e7668dd24d606e861f6c4241b434e35e850146
ElkinDev <elkinmendoza00@gmail.com>
2021-02-23 13:16:07 +1100

    this is the simple change

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -3,26 +3,8 @@ const router = express.Router();
 const db_shoppin_cart = require('../db');
 
 router.post('/', async (req,res,next)=>{
-  // Website you wish to allow to connect
-//    res.setHeader('Access-Control-Allow-Origin', '*');
-
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
 
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-
-
-
-  console.log('req',req.body)
 
   let products_in_cart=req.body.products
 
